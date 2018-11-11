26bf4a13d264bf1c9cd42ec98f91d284fb0a7843
jiajiancheng123 <634115226@qq.com>
2018-11-11 12:27:31 +0100

    restriccions 11/11

--- src/domain/Restrictions.java
+++ src/domain/Restrictions.java
@@ -1,78 +1,50 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ *
+ * @author hasee
+ */
 public class Restrictions {
+    public boolean banned;
+    public List<String> bansubjects;
+    public List<Integer> groups;
+    public List<String> banclassrooms;
     
-    /** Attributes **/
-    public Classroom[] classrooms;
-    public Timetable classTimetable;
-    public Timetable groupTimetable;
-    public GroupSubject GSNew;
-    public StudyProgram SP;
-    public boolean[] rBase;
-    
-    
-    /** Constructor **/
-    public Restrictions(Classroom[] classes, Timetable classTimetable, Timetable groupTimetable, GroupSubject GSNew) {
-        this.classrooms = classrooms;
-        this.classTimetable = classTimetable;
-        this.groupTimetable = groupTimetable;
-        this.GSNew = GSNew;
-        for (int i = 0; i < 4; i++) rBase[i] = true;
+    public Restrictions() {
+        banned = false;
+        bansubjects = new ArrayList<String>();
+        groups = new ArrayList<Integer>();
+        banclassrooms = new ArrayList<String>();
     }
     
-    public boolean base(int day, int hour) {
-        //aula no disponible
-        if(classTimetable.getGroupSubject(day, hour).isBanned() && rBase[0]) return false;
-        //se solapan dos assignaturas del mismo nivel
-        else if (sameLevel(day, hour)) return false;
-        else if (classTimetable.);
-        return true;
+    public void setBanned(boolean banned) {
+        this.banned = banned;
     }
     
-    private boolean sameLevel(int day, int hour){
-        /*return (classTimetable.getGroupSubject(day, hour).getnMat() > 0 && 
-                classTimetable.getGroupSubject(day, hour).getSubject().getLevel() == 
-                GSNew.getSubject().getLevel());*/
-        boolean overlap = false;
-        if (GSNew.getTeoria()) {
-            return groupTimetable.getGroupSubject(day, hour).getSubject().getLevel() == GSNew.getSubject().getLevel();
-        }
-        else {
-            return
-        }
+    public void addClassroom(String ref){
+        banclassrooms.add(ref);
     }
-    
-    
-    //hay case de laboratorio antes que de teoria
-    private boolean LabAfterTheroy() {
-        if (GSNew.getLab()){
-            boolean teoria = false;
-            int days = groupTimetable.getnDays();
-            int hours = groupTimetable.gethEnd() - groupTimetable.gethIni();
-            for (int i = 0; i < days; i++) {
-                for (int j = 0; j < hours; j++) 
-                    if(groupTimetable.getGroupSubject(i, j).getSubject() == GSNew.getSubject()) 
-                        teoria = groupTimetable.getGroupSubject(i,j).getTeoria();
-            }
-            if (!teoria ) return false;
-        }
-        return true;
+    public void addGroup(int num) {
+        groups.add(num);
     }
-    
-    private void banDia(int dia) {
-        if (classTimetable.getRestrictions(4)){
-            classTimetable.timeBanned(dia, groupTimetable.gethIni(), groupTimetable.gethEnd());
-        }
+    public void addSubject(String name) {
+        bansubjects.add(name);
     }
-    private void ban
-    
-    private void asdflkasdf(){}
-    
-    public add(){}
-            
-    public modify() {}
-            
-    public delete() {}
+     public void deleteClassroom(String ref){
+        banclassrooms.remove(ref);
+    }
+    public void deleteGroup(int num) {
+        groups.remove(num);
+    }
+    public void deleteSubject(String name) {
+        bansubjects.remove(name);
+    }
 }
