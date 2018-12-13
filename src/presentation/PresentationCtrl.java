857566ad6b61cba579b88964d0a2970ee4cf1e1c
raakhuga <raakhuga@raak>
2018-12-13 09:55:42 +0100

    Primeros frames GUI

--- src/presentation/PresentationCtrl.java
+++ src/presentation/PresentationCtrl.java
@@ -1,7 +1,43 @@
-
 package presentation;
 
+import domain.TimetableGenerator;
 
 public class PresentationCtrl {
+    private TimetableGenerator DomainCtrl;
     
+    //frames
+    private MainMenu mainmenu = null;
+    private StateGen stategen = null;
+    
+    public PresentationCtrl(){
+        DomainCtrl = new TimetableGenerator();
+        mainmenu = new MainMenu(this);
+        mainmenu.setVisible(true);
+    }
+    
+    //sync
+    public void SwitchFromMMtoSG(){
+        if(stategen == null)
+            stategen = new StateGen(this);
+        mainmenu.setVisible(false);
+        mainmenu.setEnabled(false);
+        stategen.setEnabled(true);
+        stategen.setVisible(true);
+    }
+    
+    public void SwitchFromSGtoMM(){
+        mainmenu.setVisible(false);
+        mainmenu.setEnabled(false);
+        stategen.setEnabled(true);
+        stategen.setVisible(true);
+    }
+    
+    //domain methods
+    public void setnMaxStudentsGroup(int nMaxStudentsGroup) {
+        DomainCtrl.setnMaxStudentsGroup(nMaxStudentsGroup);
+    }
+    
+    public void setnMaxStudentsSubgroup(int nMaxStudentsSubgroup) {
+        DomainCtrl.setnMaxStudentsSubgroup(nMaxStudentsSubgroup);
+    }
 }
