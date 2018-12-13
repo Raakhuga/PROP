0caccec99377e5ea8fd723a35347d886becedb3f
Adrian <adrian.rodriguez.aranda@est.fib.upc.edu>
2018-12-13 12:15:58 +0100

    GUI subjects

---
+++ src/presentation/SubjectsMenu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentation;
+
+/**
+ *
+ * @author adria
+ */
+public class SubjectsMenu extends javax.swing.JFrame {
+    
+    PresentationCtrl presentationctrl;
+    
+    public SubjectsManu(PresentationCtrl presentationctrl){
+        initComponents();
+        this.presentationctrl = presentationctrl;
+    }
+    
+    private void initComponents(){
+        
+    }
+    
+}
